76686429b74506a71851a680257b26bcc2b0b463
Igor <igor.ss@live.com>
2022-09-16 13:16:48 -0300

    Segundo commit até a aula 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Igor'
 
   const newName = name.toUpperCase()
 
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
       <h1>Olá Mundo!</h1>
       <p>Olá {name}</p>
       <p>Olá {newName}</p>
+      <p>Soma: {sum(1, 4)}</p>
+      <img src={url} alt="Minha Imagem" />
+      <HelloWorld />
     </div>
   );
 }
