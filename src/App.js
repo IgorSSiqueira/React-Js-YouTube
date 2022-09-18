4f4928c32025ccd9a8f9322ad1862aaad8df23ea
Igor <igor.ss@live.com>
2022-09-18 12:32:07 -0300

    Input 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
-  const name = 'Igor'
-
-  const newName = name.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
-      <p>Olá {name}</p>
-      <p>Olá {newName}</p>
-      <p>Soma: {sum(1, 4)}</p>
-      <img src={url} alt="Minha Imagem" />
-      <HelloWorld />
+      <h1>Testando Eventos</h1>
+      <Evento numero='1' /> 
+      <Evento numero='2' />
+      <Form />
     </div>
-  );
+  )
 }
 
 export default App;
